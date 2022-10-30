e42a854b60bd114507922728107edae8c97eeac3
Gopeshwar Kumawat <gopeshwark@gmail.com>
2022-10-31 00:22:27 +0530

    Created dashboard Screen

--- src/components/stat-box.jsx
+++ src/components/stat-box.jsx
@@ -22,18 +22,19 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         <Box>
           <ProgressCircle profress={progress} />
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
-            {subtitle}
-          </Typography>
-          <Typography
-            variant="h5"
-            fontWeight={"italic"}
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
+      </Box>
+
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle={"italic"}
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
